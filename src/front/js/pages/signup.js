ddf7c53a3bd2028395cca8f9091f3b39d4c0ee13
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-28 18:20:02 +0000

    Create header

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,1 +1,12 @@
-export const Signup = () => <div>Hello</div>;
+import React from "react";
+import "../../styles/signup.css";
+
+export const Signup = () => (
+  <main>
+    <header className="signup-header">
+      <div className="container text-white h-100 d-flex align-items-center">
+        <h1>NUEVO USUARIO</h1>
+      </div>
+    </header>
+  </main>
+);
