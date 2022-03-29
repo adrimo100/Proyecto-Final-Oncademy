ae4e8e1ef55b39a8f3f4ef267da1215cbfae0e57
Adrián Molina Elvira <adrimo100@gmail.com>
2022-03-29 10:16:18 +0000

    Endpoint para devolver asignaturas

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,27 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Course, Subject
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
+
+@api.route("/getCourses", methods = ["GET"])
+def getCourses():
+
+    courses_obj = Course.query.all()
+
+    if(not courses_obj):
+        return ("No hay cursos"), 400
+
+    courses = [course.serialize() for course in courses_obj]
+
+    return jsonify(courses),200
+
+
+
+
+
+
 
 
