9a64045072d529e4860c2a3dd5e8bab699de7b83
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-04-01 19:14:21 +0000

    Use subjects_ser

--- src/api/models.py
+++ src/api/models.py
@@ -31,7 +31,7 @@ class User(db.Model):
             "full_name": self.full_name,
             "email": self.email,
             "role": self.role.name,
-            "subjects": self.subjects
+            "subjects": subjects_ser
             # do not serialize the password, its a security breach
         }
 
