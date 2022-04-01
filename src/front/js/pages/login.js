b181e0e8e1f8143351b1e5485c7765786e6a55d2
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-04-01 18:30:03 +0000

    Add login page

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,3 +1,53 @@
-import React from "react";
+import { Form, Formik } from "formik";
+import React, { useContext, useState } from "react";
+import "../../styles/signup.css";
+import { TextField } from "../component/textFIeld";
+import { Context } from "../store/appContext";
 
-export const Login = () => null;
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+
+  const [formError, setFormError] = useState("");
+
+  async function hanldeSubmit(values) {
+    setFormError("");
+
+    const { error } = await actions.login(values);
+
+    if (error) setFormError(error);
+  }
+
+  return (
+    <main>
+      <header className="signup-header">
+        <div className="container text-white h-100 d-flex align-items-center">
+          <h1>LOGIN</h1>
+        </div>
+      </header>
+
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={hanldeSubmit}
+      >
+        <Form className="container py-4">
+          <TextField label="Correo electrónico" name="email" type="email" />
+          <TextField label="Contraseña" name="password" type="password" />
+
+          <button type="submit" className="btn btn-primary">
+            Iniciar sesión
+          </button>
+
+          {formError && <p className="mt-3 text-danger">{formError}</p>}
+          {store.user && (
+            <p className="mt-3 text-success">
+              ¡Bienvenido {store.user.full_name}!
+            </p>
+          )}
+        </Form>
+      </Formik>
+    </main>
+  );
+};
