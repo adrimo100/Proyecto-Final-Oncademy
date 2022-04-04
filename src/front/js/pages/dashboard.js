6fbf15bc93f6c06f0c7101c7249ba4163043eabf
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-04-04 17:52:39 +0000

    Add role-based dashboard components

--- src/front/js/pages/dashboard.js
+++ src/front/js/pages/dashboard.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import { AppHeader } from "../component/appHeader";
+import { AdminDashboard } from "../component/adminDashboard";
+import { StudentDashboard } from "../component/studentDashboard";
+import { TeacherDashboard } from "../component/teacherDashboard";
+import "../../styles/dashboard.css";
 
 export const Dashboard = () => {
+  const { store } = useContext(Context);
+
+  const role = store.user?.role;
+
   return (
     <>
-      <AppHeader>Panel de control</AppHeader>
-      <main></main>
+      <AppHeader className="dashboard-header">Panel de control</AppHeader>
+
+      {role == "Admin" && <AdminDashboard />}
+      {role == "Student" && <StudentDashboard />}
+      {role == "Teacher" && <TeacherDashboard />}
     </>
   );
 };
