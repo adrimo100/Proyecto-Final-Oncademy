dc4bebfe4a5985c84bccc9776060d8acf5ccaacd
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-04-04 18:24:15 +0000

    Use LoadingAlert

--- src/front/js/pages/dashboard.js
+++ src/front/js/pages/dashboard.js
@@ -5,15 +5,19 @@ import { AdminDashboard } from "../component/adminDashboard";
 import { StudentDashboard } from "../component/studentDashboard";
 import { TeacherDashboard } from "../component/teacherDashboard";
 import "../../styles/dashboard.css";
+import { LoadingAlert } from "../component/loadingAlert";
 
 export const Dashboard = () => {
   const { store } = useContext(Context);
 
+  const loading = false; // This will be set in the future
   const role = store.user?.role;
 
   return (
     <>
       <AppHeader className="dashboard-header">Panel de control</AppHeader>
+
+      {loading && <LoadingAlert text="Espera, estamos cargando tus datos..." />}
 
       {role == "Admin" && <AdminDashboard />}
       {role == "Student" && <StudentDashboard />}
