1fb3a0dd7d1d36dcc41618794f984829e1ad29d3
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-04-04 15:15:37 +0000

    Create AppHeader component

--- src/front/js/component/appHeader.js
+++ src/front/js/component/appHeader.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "../../styles/auth.css";
+import "../../styles/header.css";
 
-export const AuthHeader = ({ children }) => (
-  <header className="auth-header">
+export const AppHeader = ({ children, className }) => (
+  <header className={`app-header ${className ? className : ""}`}>
     <div className="app-container text-white h-100 d-flex align-items-center">
       <h1>{children}</h1>
     </div>
