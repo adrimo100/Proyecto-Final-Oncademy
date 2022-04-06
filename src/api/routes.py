b9c37e6a97a13de00a7e0b3288d60da6bf1dbc3b
Adrián Molina Elvira <adrimo100@gmail.com>
2022-04-06 19:12:44 +0000

    Endpoint de pago creado

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,11 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Course, Subject, userSubjects, Role
+from api.models import db, User, Course, Subject, userSubjects, Role, Payment
 from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
+import json
+import os
+from datetime import date
 
 import stripe
 
@@ -14,9 +16,9 @@ api = Blueprint('api', __name__)
 # Set your secret key. Remember to switch to your live secret key in production.
 # See your keys here: https://dashboard.stripe.com/apikeys
 # Clave secreta de Stripe que identifica la tienda
-stripe.api_key = "sk_test_51KjoxdLkR53a2kgbefxpdGdJYstVbITvMuqpqQfNEm7tCISSXC4j4ijVr6N82fUqsmQO48kn4ewEYvZ8V5kh51d900HguP49KH"
+stripe.api_key = os.getenv("api_key")
 
-endpoint_secret = 'whsec_6e459dd8d2484bc834066a3f7900b8bfefcc570707001c4e626034a0d3e25bbf'
+endpoint_secret = os.getenv("endpoint_secret")
 
 
 @api.route("/Courses", methods = ["GET"])
@@ -77,30 +79,92 @@ def getTeachers(subject_id):
     return jsonify(teachers), 200
 
 @api.route("/webhook", methods = ["POST"])
-def weebhook():
+def webhook():
     
-    event = None
-    payload = request.data
-    sig_header = request.headers['STRIPE_SIGNATURE']
-
-    try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, endpoint_secret
-        )
-    except ValueError as e:
-        # Invalid payload
-        raise e
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        raise e
-
+    event = request.json
+    
     # Handle the event
     if event['type'] == 'checkout.session.completed':
-      session = event['data']['object']
-    # ... handle other event types
-    else:
-      print('Unhandled event type {}'.format(event['type']))
+        session = event['data']['object']
 
-    return jsonify(success=True)
+        user = User.query.filter_by(email = event.get("data").get("object").get("customer_details").get("email")).first()
 
-   
+        if(not user):
+            return jsonify("El usuario no exite"), 404
+
+
+        splitted_url = event.get("data").get("object").get("success_url").split("/")
+
+        subject_id = int(splitted_url[4])
+
+        subject = Subject.query.filter_by(id = subject_id).first()
+
+        if(not Subject):
+            return jsonify("La asignatura no exite"), 404
+
+
+        #Añadimos el pago a la Table/Clase "Payments"
+
+        payment = Payment(date = date.now(), quantity = 20, user_id = user.id)
+
+        payment.subjects.append(subject)
+
+        db.session.add(payment)
+
+        #Añadimos el la asignatura al usuario
+
+        user.subjects.append(subject)
+
+        #Hacemos el commit
+
+        db.session.commit()
+
+    return jsonify(success=True), 200
+
+
+h = {'id': 'evt_1KlbrSLkR53a2kgbIRx5xsMY', 
+'object': 'event', 
+'api_version': '2020-08-27', 
+'created': 1649263058, 
+'data': {'object': {'id': 'cs_test_a1O8hjrWwPNqZi4ovu1kiw4bTLgRfRPgnw1KFCsqE0ZSLlPcfVc1YeTf76', 
+'object': 'checkout.session', 
+'after_expiration': None, 
+'allow_promotion_codes': None, 
+'amount_subtotal': 2000,
+'amount_total': 2000, 
+'automatic_tax': {'enabled': False, 'status': None}, 
+'billing_address_collection': None, 
+'cancel_url': 'https://3000-4geeksacademy-reactflask-ocamqqjm9fb.ws-eu38.gitpod.io/subject/6', 
+'client_reference_id': None, 
+'consent': None, 
+'consent_collection': None, 
+'currency': 'eur', 
+'customer': 'cus_LSWvGYKuDs8uRn', 
+'customer_creation': None, 
+'customer_details': {'address': {'city': None, 'country': 'ES', 'line1': None, 'line2': None, 'postal_code': None, 'state': None}, 
+'email': 'adrimo100@gmail.com', 
+'name': 'Adrián Molina Elvira', 
+'phone': None, 
+'tax_exempt': 'none', 
+'tax_ids': []}, 
+'customer_email': None, 
+'expires_at': 1649349426, 
+'livemode': False, 'locale': None, 
+'metadata': {},
+'mode': 'subscription', 
+'payment_intent': None, 'payment_link': 
+None, 'payment_method_options': None, 
+'payment_method_types': ['card'], 
+'payment_status': 'paid', 
+'phone_number_collection': {'enabled': False}, 
+'recovered_from': None, 
+'setup_intent': None, 'shipping': None, 
+'shipping_address_collection': None, 'shipping_options': [], 
+'shipping_rate': None, 'status': 
+'complete', 'submit_type': None, 
+'subscription': 'sub_1KlbrFLkR53a2kgbJlCbyq7K', 
+'success_url': 'https://3000-4geeksacademy-reactflask-ocamqqjm9fb.ws-eu38.gitpod.io/subject/6/success', 
+'total_details': {'amount_discount': 0, 'amount_shipping': 0, 'amount_tax': 0}, 'url': None}}, 
+'livemode': False, 'pending_webhooks': 1, 
+'request': {'id': 'req_XhSBiUqYhfquMt', 'idempotency_key': '284d3aa1-04a7-479d-afe0-c04c84201c49'}, 
+'type': 'checkout.session.completed'}
