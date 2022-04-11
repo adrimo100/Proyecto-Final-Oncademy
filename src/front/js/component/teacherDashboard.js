084bfe507fcb14fd136169e34df8f9817909d992
Adrián Molina Elvira <adrimo100@gmail.com>
2022-04-11 17:37:43 +0000

    Teacher Dashboard 1.0

--- src/front/js/component/teacherDashboard.js
+++ src/front/js/component/teacherDashboard.js
@@ -1,8 +1,34 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 
+import { SubjectCard } from "../component/subjectCard";
+
 export const TeacherDashboard = () => {
   const { store, actions } = useContext(Context);
+
+  const displaySubjects = () => {
+    if (store.user?.subjects.lenght == 0)
+      return (
+        <div className="mt-4">
+          <strong>
+            <li>No Tienes Asignaturas Asignadas</li>
+          </strong>
+        </div>
+      );
+    else
+      return (
+        <div className="mt-4">
+          <div
+            className="row mt-4 d-flex align-items-center"
+            id="subjects-cards"
+          >
+            {store.user?.subjects.map((subject, index) => (
+              <SubjectCard key={index} subject={subject}></SubjectCard>
+            ))}
+          </div>
+        </div>
+      );
+  };
 
   return (
     <main className="app-container container-fluid py-4">
@@ -16,6 +42,13 @@ export const TeacherDashboard = () => {
       <div className="d-flex">
         <strong>Email:</strong>
         <p className="ps-1">{store.user?.email}</p>
+      </div>
+
+      <hr />
+
+      <div>
+        <h2>Tus Asignaturas Asignadas</h2>
+        <div>{displaySubjects()}</div>
       </div>
     </main>
   );
