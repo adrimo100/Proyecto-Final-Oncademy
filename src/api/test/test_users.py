762eb51d896b59fddb43174a269d51b01cce6552
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-04-14 19:59:53 +0200

    Rewrite tests

--- src/api/test/test_users.py
+++ src/api/test/test_users.py
@@ -9,140 +9,102 @@ fake = Faker("es_ES")
 
 
 @pytest.fixture
-def admin(roles):
-    admin = User(
-        full_name=fake.name(),
-        email=fake.email(),
-        password=fake.password(),
-        role=roles["Admin"],
-    )
-    db.session.add(admin)
-    db.session.commit()
+def create_users(roles):
+    generated_users = []
 
-    yield admin
+    def _create_users(*roles_count):
+        """Takes a list of tuples (role, count) and create users with the specified role.
+        Returns a list of users. Cleans up the users after the test."""
+        users = []
 
-    db.session.delete(admin)
-    db.session.commit()
-
-
-@pytest.fixture
-def student(roles):
-    student = User(
-        full_name=fake.name(),
-        email=fake.email(),
-        password=fake.password(),
-        role=roles["Student"],
-    )
-    db.session.add(student)
-    db.session.commit()
-
-    yield student
-
-    db.session.delete(student)
-    db.session.commit()
-
-
-@pytest.fixture(
-    params=[
-        # No results (when searching for a those roles)
-        {"Student": 0, "Teacher": 0},
-        # One result and one page
-        {"Student": 1, "Teacher": 1},
-        # Multiple results and pages
-        {"Student": 17, "Teacher": 17},
-    ],
-    ids=["no_addition", "one_addition", "multiple_additions"],
-)
-def create_users(roles, request):
-    added_users = []
-
-    for role, count in request.param.items():
-        for _ in range(count):
-            added_users.append(
-                User(
-                    full_name=fake.name(),
-                    email=fake.email(),
-                    password=fake.password(),
-                    role=roles[role],
+        for role, count in roles_count:
+            for i in range(count):
+                users.append(
+                    User(
+                        full_name=fake.name(),
+                        email=fake.email(),
+                        password=fake.password(),
+                        role=roles[role],
+                    )
                 )
-            )
 
-    db.session.add_all(added_users)
-    db.session.commit()
+        db.session.add_all(users)
+        db.session.commit()
+        generated_users.extend(users)
 
-    yield added_users
+        return users
 
-    for user in added_users:
+    yield _create_users
+
+    for user in generated_users:
         db.session.delete(user)
     db.session.commit()
 
 
 @pytest.fixture
-def get_db_users():
-    return lambda: User.query.all()
-
-
-@pytest.fixture
-def authorization():
+def get_authorization_header():
     """Returns the authorization header for a user"""
     return lambda user: {
         "Authorization": f"Bearer {create_access_token(identity=user.id)}"
     }
 
+    
 
-@pytest.fixture
-def get_users_as_admin(client, admin, authorization):
-    return lambda page=1: client.get(f"/api/users?page={page}", headers={**authorization(admin)})
-
-
-class TestUsers:
-    """
-    Test the users API
-    """
-
-    def test_get_users(self, create_users, get_users_as_admin, get_db_users):
-        """
-        Test that the users API returns a list of users
-        """        
-        response = get_users_as_admin()
-        db_users = get_db_users()
-
-        assert response.status_code == 200
-        assert response.json["total"] == len(db_users)
-        assert response.json["pages"] == math.ceil(len(db_users) / 10)
-        for response_user in response.json["users"]:
-            assert response_user in [db_user.serialize() for db_user in db_users]
-
-    def test_pagination(self, create_users, get_users_as_admin, get_db_users):
-        """
-        Test that we can get all the users with pagination
-        """        
-        db_users = get_db_users()
-        total_pages = math.ceil(len(db_users) / 10)
-        fetched_users = set()
-
-        for page in range(1, total_pages + 1):
-            response = get_users_as_admin(page)
-            for user in response.json["users"]:
-                fetched_users.add(user["id"])
-            
-        assert len(fetched_users) == len(db_users)
-
-    @pytest.mark.skip(reason="TODO")
-    def test_filter_by_role():
-        # TODO
-        pass
-
-    def test_fail_without_token(self, client):
-        """
-        Test that the users API returns a 401 if no token is provided
-        """
+class TestGetUsersInvalidRole:
+    def test_get_unauthenticated_should_fail(self, client):
         response = client.get("/api/users")
-
         assert response.status_code == 401
 
-    def test_fail_as_student(self, client, student, authorization):
-        response = client.get("/api/users", headers={**authorization(student)})
+    @pytest.mark.parametrize("role", ["Student", "Teacher"])
+    def test_get_as_student_or_teacher_should_fail(
+        self, client, role, create_users, get_authorization_header
+    ):
+        user = create_users((role, 1)).pop()
+
+        response = client.get("api/users", headers=get_authorization_header(user))
+
         assert response.status_code == 403
 
+
+class TestGetUsersSuccess:
+    def test_get_users_success(self, client, create_users, get_authorization_header):
+        users = create_users(("Admin", 1), ("Student", 5))
+        admin = next(user for user in users if user.role.name == "Admin")
+
+        response = client.get(
+            "/api/users", headers=get_authorization_header(admin)
+        )
+
+        assert response.status_code == 200
+        assert len(response.json["users"]) == len(users)
+
+    def test_get_users_implements_pagination(self, client, create_users, get_authorization_header):
+        users = create_users(("Admin", 1), ("Student", 10), ("Teacher", 10))
+        admin = next(user for user in users if user.role.name == "Admin")
+
+        response = client.get(
+            "/api/users?page=2", headers=get_authorization_header(admin)
+        )
+
+        assert response.status_code == 200
+        assert len(response.json["users"]) == 10
+        assert response.json["pages"] == math.ceil(len(users) / 10) # 3 pages
+
+    def test_get_all_users_using_pagination(self, client, create_users, get_authorization_header):
+        users = create_users(("Admin", 1), ("Student", 10), ("Teacher", 10))
+        admin = next(user for user in users if user.role.name == "Admin")
+        expected_pages = math.ceil(len(users) / 10)
+
+        user_ids_from_api = []
+        for page in range(1, expected_pages + 1):
+            response = client.get(
+                f"/api/users?page={page}", headers=get_authorization_header(admin)
+            )
+            user_ids_from_api.extend(
+                [user["id"] for user in response.json["users"]]
+            )
+
+        for user in users:
+            assert user.id in user_ids_from_api
+        
 
