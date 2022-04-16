d3607967e6fe738eed0329265a487b7b6e145d01
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-04-16 10:48:52 +0200

    Refactor tests

--- src/api/test/test_users.py
+++ src/api/test/test_users.py
@@ -78,20 +78,27 @@ class TestGetUsersFailure:
 
 
 class TestGetUsersSuccess:
-    def test_success(self, client, create_users, get_authorization_header):
-        users = create_users(("Admin", 1), ("Student", 5))
-        admin = next(user for user in users if user.role.name == "Admin")
+    # Arrange
+    @pytest.fixture()
+    def users(self, create_users):
+        return create_users(("Admin", 1), ("Student", 15), ("Teacher", 15))
 
+    # Arrange
+    @pytest.fixture()
+    def admin(self, users):
+        return next(user for user in users if user.role.name == "Admin")
+
+    def test_success(self, client, users, admin, get_authorization_header):
         response = client.get("/api/users", headers=get_authorization_header(admin))
 
         assert response.status_code == 200
-        assert len(response.json["users"]) == len(users)
+        for user in response.json["users"]:
+            assert user["id"] in [user.id for user in users]        
 
     def test_implements_pagination(
-        self, client, create_users, get_authorization_header
+        self, client, users, admin, get_authorization_header
     ):
-        users = create_users(("Admin", 1), ("Student", 10), ("Teacher", 10))
-        admin = next(user for user in users if user.role.name == "Admin")
+        expected_total_pages = math.ceil(len(users) / 10)
 
         response = client.get(
             "/api/users?page=2", headers=get_authorization_header(admin)
@@ -99,34 +106,34 @@ class TestGetUsersSuccess:
 
         assert response.status_code == 200
         assert len(response.json["users"]) == 10
-        assert response.json["pages"] == math.ceil(len(users) / 10)  # 3 pages
+        assert response.json["pages"] == expected_total_pages
+        assert response.json["total"] == len(users)
 
     def test_get_all_users_using_pagination(
-        self, client, create_users, get_authorization_header
+        self, client, users, admin, get_authorization_header
     ):
-        users = create_users(("Admin", 1), ("Student", 10), ("Teacher", 10))
-        admin = next(user for user in users if user.role.name == "Admin")
         expected_pages = math.ceil(len(users) / 10)
 
         user_ids_from_api = []
+        # Get all pages from api and store the id of each user
         for page in range(1, expected_pages + 1):
             response = client.get(
                 f"/api/users?page={page}", headers=get_authorization_header(admin)
             )
             user_ids_from_api.extend([user["id"] for user in response.json["users"]])
 
+        # Assert that all users were fetched from the api
         for user in users:
             assert user.id in user_ids_from_api
 
     @pytest.mark.parametrize("role", ["Student", "Teacher"])
-    def test_filter_by_role(self, client, create_users, get_authorization_header, role):
-        users = create_users(("Admin", 1), ("Student", 15), ("Teacher", 15))
-        admin = next(user for user in users if user.role.name == "Admin")
-
+    def test_filter_by_role(self, client, users, admin, get_authorization_header, role):
+        expected_users_count = len([user for user in users if user.role.name == role])
+        
         response = client.get(
             f"/api/users?role={role}", headers=get_authorization_header(admin)
         )
 
         assert response.status_code == 200
-        assert response.json["total"] == 15
+        assert response.json["total"] == expected_users_count
 
