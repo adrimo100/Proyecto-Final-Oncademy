b7fdefe53860e3a2b810d36168a755d32a152814
Adrián Molina Elvira <adrimo100@gmail.com>
2022-04-27 18:35:36 +0000

    Arreglados fallos estéticos

--- src/front/js/component/editUser.js
+++ src/front/js/component/editUser.js
@@ -22,7 +22,7 @@ export const EditUser = () => {
     if (!editName)
       return (
         <div className="d-flex">
-          <p className="ps-1 m-0">{store.user?.full_name}</p>
+          <p className="ps-1 mt-1 mb-1">{store.user?.full_name}</p>
           <div className="m-0 p-0">
             <button
               className="btn btn-whites ms-1 p-0"
@@ -92,7 +92,7 @@ export const EditUser = () => {
     if (!editEmail)
       return (
         <div className="d-flex">
-          <p className="ps-1 m-0">{store.user?.email}</p>
+          <p className="ps-1 mt-1 mb-1">{store.user?.email}</p>
           <div className="m-0 p-0">
             <button
               className="btn btn-whites ms-1 p-0"
